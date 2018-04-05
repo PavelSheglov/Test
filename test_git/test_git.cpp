a45ccc9c3c2d1f5ef1fa7102100f65399487d747
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-05 21:42:54 +0700

    Создание новой ветки с вводом имени

--- test_git/test_git.cpp
+++ test_git/test_git.cpp
@@ -6,8 +6,11 @@
 
 void main()
 {
+	char name[80];
+	printf("Enter your name:");
+	scanf("%s", name);
 	printf("\n-----------------------------------------\n");
-	printf("Hello, world! This is test git program.\n");
+	printf("Hello, %s! This is test git program.", name);
 	printf("\n-----------------------------------------\n");
 	printf("Press any key for exit...\n");
 	_getch();
