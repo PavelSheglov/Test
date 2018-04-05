614fea9116d40c2e584eee71cfd362826ac87e30
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-05 23:27:39 +0700

    Убран перенос строки

--- test_git/test_git.cpp
+++ test_git/test_git.cpp
@@ -13,7 +13,7 @@ void main()
 	printf("Enter your name:");
 	scanf("%s", name);
 	printf("\n-----------------------------------------\n");
-	printf("Hello, %s! This is test git program.\n", name);
+	printf("Hello, %s! This is test git program.", name);
 	printf("\n-----------------------------------------\n");
 	printf("Press any key for exit...\n");
 	_getch();
